c6c7b87c6a356049dedd15c5868313576e458211
Domenic Perriccioli <domenic.perriccioli@hotmail.com>
2022-06-27 23:32:18 -0400

    stuck at upgrading with data volume

--- unifi-video/entrypoint.sh
+++ unifi-video/entrypoint.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-mongod --fork --logpath /var/log/mongod.log --dbpath /data
+function graceful_shutdown {
+  echo -n "Stopping unifi-video... "
+  if /usr/sbin/unifi-video --nodetach stop; then
+    echo "done."
+    exit 0
+  else
+    echo "failed."
+    exit 1
+  fi
+}
 
-unifi-video -D start
+# Trap SIGTERM for graceful exit
+trap graceful_shutdown SIGTERM
+
+mongod --fork --logpath /var/log/mongod.log --dbpath /db-data
+
+# Run the unifi-video daemon the unifi-video way
+echo -n "Starting unifi-video... "
+if /usr/sbin/unifi-video --debug start; then
+  echo "done."
+else
+  echo "failed."
+  exit 1
+fi
+
+while true; do
+  sleep 3
+done
